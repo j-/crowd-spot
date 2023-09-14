9af26c34c173dc102c2dd602955d407480345c6f
j- <j@skeoh.com>
2023-09-14 11:23:46 +1000

    Make the top color of the favicon 1px taller to decrease gap

--- src/components/IconColors.tsx
+++ src/components/IconColors.tsx
@@ -15,8 +15,8 @@ export const IconColors: FC<IconColorsProps> = ({ color1, color2 }) => (
       </>
     ) : (
       <>
-        <path fill="#fff" d="M1 1H15V7H1zM1 9H15V15H1z" />
-        <path fill={color1} d="M2 2H14V6H2z" />
+        <path fill="#fff" d="M1 1H15V8H1zM1 9H15V15H1z" />
+        <path fill={color1} d="M2 2H14V7H2z" />
         <path fill={color2} d="M2 10H14V14H2z" />
       </>
     )}
