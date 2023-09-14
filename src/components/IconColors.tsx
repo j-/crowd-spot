fd787756e522d5a273161d01c6ecb02e21b20e9c
j- <j@skeoh.com>
2023-09-14 11:12:19 +1000

    Fix favicon to have crisp edges

--- src/components/IconColors.tsx
+++ src/components/IconColors.tsx
@@ -9,11 +9,16 @@ export const IconColors: FC<IconColorsProps> = ({ color1, color2 }) => (
   <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16">
     <path fill="#000" d="M0 0H16V16H0z" />
     {!color2 ? (
-      <path fill={color1} d="M1 1H15V15H1z" stroke="white" strokeWidth={0.5} />
+      <>
+        <path fill="#fff" d="M1 1H15V15H1z" />
+        <path fill={color1} d="M2 2H14V14H2z" />
+      </>
     ) : (
       <>
-        <path fill={color1} d="M1 1H15V7.5H1z" stroke="white" strokeWidth={0.5} />
-        <path fill={color2} d="M1 9H15V15H1z" stroke="white" strokeWidth={0.5} />
+        <path fill="#fff" d="M1 1H15V7H1z" />
+        <path fill={color1} d="M2 2H14V6H2z" />
+        <path fill="#fff" d="M1 9H15V15H1z" />
+        <path fill={color2} d="M2 10H14V14H2z" />
       </>
     )}
   </svg>
