0657d0ccd059832bfd91917e783b70f78cd07856
j- <j@skeoh.com>
2024-05-26 13:22:40 +1000

    Add umami

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,7 @@ import { AppStateProvider } from '@/components/AppStateProvider';
 import '@/globals.css';
 import type { AppType } from 'next/app';
 import Head from 'next/head';
+import Script from 'next/script';
 
 const App: AppType = ({ Component, pageProps }) => {
   return (
@@ -10,6 +11,7 @@ const App: AppType = ({ Component, pageProps }) => {
         <title>Crowd Spot</title>
       </Head>
       <Component {...pageProps} />
+      <Script defer src="https://skeoh-umami.fly.dev/script.js" data-website-id="a5c13983-7415-4b75-be55-0ca0d5181134" />
     </AppStateProvider>
   );
 };
